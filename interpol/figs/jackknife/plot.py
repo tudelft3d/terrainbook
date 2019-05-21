81a83a080bbd5a7cb07b656d4d285d454b5d5c87
Hugo Ledoux <h.ledoux@tudelft.nl>
2019-05-21 15:32:35 +0200

    Finish draft of jackknife of the bloody figures
    matplotlib is time-consuming!

--- interpol/figs/jackknife/plot.py
+++ interpol/figs/jackknife/plot.py
@@ -28,10 +28,8 @@ fig, ax = plt.subplots()
 
 # im0 = mpimg.imread('/Users/hugo/teaching/terrainbook/interpol/figs/jackknife/dem_01_preview.tiff')
 # imgplot = ax.imshow(im0)
-# ax.get_xaxis().set_visible(False)
-# ax.get_yaxis().set_visible(False)
 
-# im0 = ax.scatter(pts[:,0], pts[:,1], c=pts[:,2], alpha=0.5, cmap='Reds')
+# im0 = ax.scatter(pts[:,0], pts[:,1], c=pts[:,2], alpha=0.5)
 
 # im0 = ax.scatter(pts[:,0], pts[:,1], c=cm, alpha=0.5, cmap='Reds')
 
@@ -40,13 +38,21 @@ im0 = ax.scatter(pts[:,2], pts[:,3], c=cm, alpha=0.5, cmap='Reds')
 
 # ax[0].legend()
 # ax.grid(True)
-# ax[0].set_xlabel(r'$x$', fontsize=10)
-# ax[0].set_ylabel(r'$y$', fontsize=10)
+ax.set_xlabel(r'Real value', fontsize=10)
+ax.set_ylabel(r'Estimated value', fontsize=10)
 # ax[0].set_title('RMSE from IDW')
 
+# ax.get_xaxis().set_visible(False)
+# ax.get_yaxis().set_visible(False)
 fig.colorbar(im0)
 
+# locs, labels = xticks()
+# print (labels)
+# ax.set_xticklabels( ('a', '2.5', '5.0', '7.5', '10.0', '12.5', '15.0', '17.5', '20.0', '22.5', '1') )
+# ax.set_yticklabels( ('a', '2.5', '5.0', '7.5', '10.0', '12.5', '15.0', '17.5', '20.0', '22.5', '1') )
+
 # plt.show()
+# plt.savefig("aaa.pdf", bbox_inches='tight')
 plt.savefig("jk4.pdf", bbox_inches='tight')
 
 
@@ -56,7 +62,7 @@ plt.savefig("jk4.pdf", bbox_inches='tight')
 # import sys
 
 # tmp = []
-# with open('jk3.xyz') as csvfile:
+# with open('jk2.xyz') as csvfile:
 #     r = csv.reader(csvfile, delimiter=' ')
 #     # header = next(r)
 #     for line in r:
